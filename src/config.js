d801eb363a5bde8a426436ee92a2724d07496269
최준영 <choejunyeong59@gmail.com>
2026-07-24 22:10:39 +0900

    Expand privacy policy and terms of service

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ export const SITE_CONFIG = {
   siteUrl: "https://cjy.app",
   developerName: "CJY",
   contactEmail: "cjy.support@gmail.com",
-  supportEmail: "support@cjy.app",
+  supportEmail: "cjy.support@gmail.com",
   instagramUrl: "https://www.instagram.com/cjy.app/",
   appStoreUrl: "https://apps.apple.com/app/id0000000000",
   musicNowName: "Music Now",
-  lastUpdated: "2026-06-02"
+  lastUpdated: "2026-07-24"
 };
